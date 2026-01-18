a797b5fe1735073985023a2fe9d90b07e8065619
Darius Valancauskas <darius@wiz-team.com>
2026-01-18 19:12:13 +0200

    OutputProcessorTest 2 cases

--- tests/Generator/OutputProcessorTest.php
+++ tests/Generator/OutputProcessorTest.php
@@ -8,29 +8,54 @@ use App\Converter\ConverterInterface;
 use App\Generator\OutputProcessor;
 use App\Generator\Strategies\OutputStrategyInterface;
 use App\Services\Printer;
+
 use PHPUnit\Framework\TestCase;
 
 class OutputProcessorTest extends TestCase
 {
-    public function testItCallsSupportingStrategy(): void
+    public function testFirstStrategySupports(): void
     {
         $generatedValue = 'hello';
         $convertedValue = 'converted';
 
         $printer = $this->createStub(Printer::class);
-
         $converter = $this->createStub(ConverterInterface::class);
         $converter->method('convert')->with($generatedValue)->willReturn($convertedValue);
 
-        $strategy = $this->createMock(OutputStrategyInterface::class);
-        $strategy->method('supports')->with($generatedValue)->willReturn(true);
-        $strategy->expects($this->once())
+        $firstStrategy = $this->createMock(OutputStrategyInterface::class);
+        $firstStrategy->method('supports')->with($generatedValue)->willReturn(true);
+        $firstStrategy->expects($this->once())
             ->method('process')
-            ->with($generatedValue, $converter, $printer)
-        ;
+            ->with($generatedValue, $converter, $printer);
 
-        $processor = new OutputProcessor([$strategy], $printer);
+        $secondStrategy = $this->createMock(OutputStrategyInterface::class);
+        $secondStrategy->method('supports')->with($generatedValue)->willReturn(false);
+        $secondStrategy->expects($this->never())->method('process');
 
+        $processor = new OutputProcessor([$firstStrategy, $secondStrategy], $printer);
+        $processor->process($generatedValue, $converter);
+    }
+
+    public function testSecondStrategySupports(): void
+    {
+        $generatedValue = 'hello';
+        $convertedValue = 'converted';
+
+        $printer = $this->createStub(Printer::class);
+        $converter = $this->createStub(ConverterInterface::class);
+        $converter->method('convert')->with($generatedValue)->willReturn($convertedValue);
+
+        $firstStrategy = $this->createMock(OutputStrategyInterface::class);
+        $firstStrategy->method('supports')->with($generatedValue)->willReturn(false);
+        $firstStrategy->expects($this->never())->method('process');
+
+        $secondStrategy = $this->createMock(OutputStrategyInterface::class);
+        $secondStrategy->method('supports')->with($generatedValue)->willReturn(true);
+        $secondStrategy->expects($this->once())
+            ->method('process')
+            ->with($generatedValue, $converter, $printer);
+
+        $processor = new OutputProcessor([$firstStrategy, $secondStrategy], $printer);
         $processor->process($generatedValue, $converter);
     }
 }
